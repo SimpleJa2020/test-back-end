fa4ac32e67f483731e12591584cdba09a723f476
SimpleJa2020 <ja_37@hotmail.co.th>
2023-04-21 15:05:43 +0700

    config essential middleware

--- src/app.js
+++ src/app.js
@@ -1,2 +1,27 @@
 // const { sequelize } = require('./models');
 // sequelize.sync({ force: true });
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const notFoundMiddleware = require('./middlewares/not-found');
+const errorMiddleware = require('./middlewares/error');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 9000;
+app.listen(port, () =>
+    console.log(chalk.bgBlue.bold(`server running on port: ${port}`))
+);
